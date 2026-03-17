2de08f34d929d79f6a97f3c799e89a265e193548
Sanjay Sakthivel <sanjay.sakthivel@aspiresys.com>
2026-03-17 22:49:37 +0530

    16.03-2026 and 17-03-2026

--- 16-03-2026.js
+++ 16-03-2026.js
@@ -13,7 +13,6 @@
 
 // const add=(a,b)=>{
 //   return a+b;
-  
 // }
 
 // console.log(add(2,5));
@@ -99,6 +98,8 @@
 // }
 // console.log(getArr([1,2,3,4,4,3]));
 
+
+
 // const stu = students.map((item)=>{
 //   const [names,age,subject]= item
 //   return{
@@ -147,7 +148,7 @@ class Bank{
     }
     withDraw(amount){
         if(amount<=this.#balanceAccount)
-            this.#balanceAccount-= amount;
+            this.#balanceAccount -= amount;
     }
 
     getAmount(){
@@ -173,13 +174,17 @@ class Loan{
 }
 
 class HomeLoan extends Loan{
+    // constructor(carLoan){
+    //     super(carLoan)
+    // }
     HomeL(){
-        console.log(`${this.homeLoan} this is car loan`)
+        console.log(`${this.homeLoan} this is Home loan `)
     }
 }
 
 const loan= new HomeLoan(12000,20000);
 loan.HomeL();
+loan.carL();
 
 // Polymorphism => It is to write the same methods and behaves different in different class
 
@@ -206,6 +211,256 @@ const phone= new Phone()
 const mi = new Redmi();
 mi.camera()
 phone.camera()
+
+// Abstraction => It shows only essestial features and hide the implementation details.
+
+class Payment{
+    pay(amount){
+        console.log("Paying or paid");
+        
+    }
+
+}
+class CreditCard extends Payment{
+    pay(amount){
+        console.log("Credit card amount paid"+amount);
+        
+    }
+}
+class Gpay extends Payment{
+    pay(amount){
+        console.log("Payment Via Gpay paid"+amount);
+        
+    }
+}
+const paying = new CreditCard();
+paying.pay(12345)
+ 
+const nums= [1,2,3,4,5,6,7,8];
+const newres= nums.filter((item, index)=>index%2==0).map(item=>item*2);
+console.log(newres);
+
+// const order= students.sort((a,b)=>b[1]-a[1]);
+// console.log(order);
+
+function getDuplicates(nums) {
+  return nums.filter((item, index) => nums.indexOf(item) !== index);
+}
+console.log(getDuplicates([1,2,3,4,4,3]));
+
+// Q: Get total price of completed orders
+
+const users = [
+  {
+    name: "Sanjay",
+    orders: [
+      {price: 100, status: "completed"},
+      {price: 200, status: "pending"}
+    ]
+  },
+  {
+    name: "Rahul",
+    orders: [
+      {price: 300, status: "completed"}
+    ]
+  }
+]
+
+// cal total price
+const total= users.reduce((acc,item)=>{
+    item.orders.forEach(item=>{
+        acc+=item.price
+    })
+    return acc;
+},0)
+console.log(total)
+
+// const arr = ["a","b","c"];
+// const ind1= arr.map((item,index,arr)=>{
+//     return item+index
+// })
+// console.log(ind1);
+
+// const arr = [10,20,30];
+// const ind2= arr.map((item,index)=>{
+//     return index*item;
+// })
+// console.log(ind2);
+
+// const arr = [5,10,15,20,25];
+// const ind3= arr.filter((item,index)=>index%2==0)
+// console.log(ind3);
+
+// const arr = ["a","b","c","d"];
+// const ind4= arr.map((item,index)=>{
+//     if(index%2!==0){
+//         return "X"
+//     }
+//     return item
+// })
+// console.log(ind4);
+
+const userss = [{name:"Sanjay"},{name:"Rahul"}];
+const ind5= userss.map((item,index)=>{
+    return {
+        name:item.name,
+        id:index,
+    }
+})
+console.log(ind5);
+
+// const arr = [10,20,30,40];
+// const ind6= arr.reduce((acc,item,index)=>{
+//     if(index%2!==0){
+//         acc+=item
+//     }
+//     return acc;
+// },0)
+// console.log(ind6);
+
+// const arr = [0,2,2,3];
+// const ind7= arr.find((item,index)=>index===item)
+// console.log(ind7);
+
+// const arr = [1,2,3,4,5,6,7];
+// const ind8= arr.filter((item,index)=>{
+//     return index%3!==0
+// })
+// console.log(ind8);
+
+
+// const arr = ["a","b","c"];
+// const ind9= arr.reduce((acc,item,index)=>{
+//     acc[index]=item
+//     return acc;
+// },{})
+// console.log(ind9)
+
+const arr = [10,20,30,40,50];
+
+const ind10= arr.reduce((acc,item,index)=>{
+    if(index %2===0){
+        acc.EvenIndex.push(item)
+    }else{
+        acc.OddIndex.push(item)
+    }
+    return acc;
+},{EvenIndex:[],OddIndex:[]})
+console.log(ind10);
+
+const students = [
+  ["John", 85, "Math"],
+  ["Sara", 92, "Science"],
+  ["Mike", 70, "History"],
+  ["Anna", 60, "Math"],
+  ["Tom", 95, "Science"]
+];
+
+const new1= students.map(item=>item[0]);
+console.log(new1);
+
+const new2= students.map((item,index)=>{
+    return{
+        id:index,
+        name:item[0],
+        age:item[1],
+        subjects:item[2]
+    }
+})
+console.log(new2);
+
+const new3= students.filter((item,index)=>{
+    return index%2===0
+})
+console.log(new3);
+
+const new4= students.reduce((acc,item,index)=>{
+    return acc[item[1]]+index
+},0)
+console.log(new4);
+
+const new5= students.reduce((acc,item,index)=>{
+    if(index%2!==0){
+        acc[item[1]]="Unknown"
+    }
+    return acc
+},{})
+console.log(new5);
+
+const new6= students.reduce((acc,item,index)=>{
+    if(index%2!==0){
+         acc+=item[1]
+    }
+    return acc;
+},0)
+console.log(new6);
+
+// const values= [1,2,3,4,5];
+// const rev= values.reverse();
+// console.log(rev);
+
+//Reverse only elements at even indexes
+const values=[1,2,3,4,5];
+const new7= values.filter((item,index,array)=>index%2==0)
+const new8= new7.reverse()
+console.log(new8);
+
+
+// Remove Duplicates from array of objects
+
+const products = [
+  { id: 1, name: "Watch" },
+  { id: 2, name: "Shoes" },
+  { id: 1, name: "RRRR" }
+];
+
+const new9= [
+    ...new Map(
+        products.map(item=>[item.id,item])
+    ).values()
+]
+console.log(new9);
+
+// Removing dupicates
+const usersss = ["a@gmail.com", "b@gmail.com", "a@gmail.com"];
+const res1= [...new Set(usersss)]
+console.log(res1);
+
+const visited = new Set();
+
+function visitPage(page){
+    if(visited.has(page)){
+        console.log("Page has been already visited");
+    }else{
+        visited.add(page);
+        console.log("Visited page");
+    }
+}
+visitPage("Users")
+visitPage("Users")
+visitPage("Home")
+
+const items = ["apple", "banana", "apple", "orange", "banana"];
+const count=new Map();
+items.forEach(item => {
+    count.set(item,(count.get(item)||0)+1);
+});
+console.log(count);
+
+const data = [
+  { name: "A", role: "admin" },
+  { name: "B", role: "user" },
+  { name: "C", role: "admin" }
+];
+const group = new Map();
+data.forEach(item=>{
+    const key =item.role;
+    const arr=group.get(key) ||[];
+    arr.push(item)
+    group.set(key,arr);
+})
+console.log(group);
+
 
 
 
---
+++ 17-03-2026.js
@@ -0,0 +1,59 @@
+//Practice
+const arr1=[1,2,3,4,4,3,2]
+const removeDup= new Set(arr1) 
+console.log(removeDup);
+
+const str= "javascript"
+const newset= new Set(str);
+console.log(newset);
+
+const arr2=["a","b","a","c","b","a"]
+const grp= new Map();
+
+arr2.forEach(item=>{
+    grp.set(item,(grp.get(item)||0)+1)
+})
+console.log(grp);
+
+function findDuplicates(arr) {
+  return [...new Set(arr.filter((item, index) => arr.indexOf(item) !== index))];
+}
+console.log(findDuplicates([1,2,3,4,4,3,5]));
+
+const strArr= ["hi","hello","hey","world"]
+
+const grpMap= new Map()
+
+strArr.forEach(item=>{
+    const key= item.length
+    const arr= grpMap.get(key)||[]
+    arr.push(item)
+    grpMap.set(key,arr)
+})
+console.log(grpMap);
+
+// Input: [1,2,3,4,5], target = 6
+const intArr= [1,2,3,4,5]
+const target=6;
+
+function findPair(arr, target) {
+for(let i=0;i<intArr.length;i++){
+    for(let j=i+1;j<intArr.length;j++){
+        if(intArr[i]+intArr[j]===target){
+            return [arr[i], arr[j]];
+        }
+    }
+    
+}
+return null;
+}
+console.log(findPair(intArr,target));
+
+
+
+
+
+
+
+
+
